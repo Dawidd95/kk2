b09ca4dd4b2fd9ca881fc82689bac91dfbd66794
Dawidd95 <32812931+Dawidd95@users.noreply.github.com>
2018-08-01 09:30:47 +0200

    Add files via upload

---
+++ scripts/scroll-reveal.js
@@ -0,0 +1,9 @@
+var mobileViewport = window.matchMedia("screen and (max-width: 1023px)");
+      
+if(mobileViewport.matches) {
+   	window.sr = ScrollReveal();   //deklaracja biblioteki
+    sr.reveal('.quotes__wrapper',{duration: 0});  //co chcemy animowac
+} else {
+    window.sr = ScrollReveal();   //deklaracja biblioteki
+    sr.reveal('.quotes__wrapper',{duration: 1000});  //co chcemy animowac
+}
